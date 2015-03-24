18f254499754285a79d0575e97553c48b3cb83a6
u12003922 <u12003922@tuks.co.za>
2015-03-24 23:12:55 +0200

    Converting to Node
    Added each file to there function

--- Reporting.js
+++ Reporting.js
@@ -6,6 +6,17 @@ var myCallback = function(data) {
         console.log('got data: '+data);
     };
 
+/**
+ * The functions of the reporting module are loaded into each of these variables.
+ * Use the variable to call the function.
+ * Example : getThreadStats(@parameters);
+ * @type {exports}
+ */
+var getThreadStats = require('./ReportingB/getThreadStats.js');
+var getThreadAppraisal = require('./ReportingB/getThreadAppraisal.js');
+var exportThreadAppraisal = require('./ReportingB/exportThreadAppraisal.js');
+var importThreadAppraisal = require('./ReportingB/importThreadAppraisal.js');
+var exportThread = require('./ReportingB/exportThread.js');
 var importThread = require('./ReportingB/importThread.js');
 importThread("C:/New folder","backup.txt",myCallback);
 
