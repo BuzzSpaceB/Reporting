9680d615e757f1c8d99722d43628494ab3bb8146
JamesJJMurray <jjjmurray@gmail.com>
2015-03-24 22:07:33 +0200

    Starting to make module node-usable

---
+++ Reporting.js
@@ -0,0 +1,7 @@
+/**
+ * Created by User on 3/24/2015.
+ */
+var importThread = require('./ReportingB/importThread.js');
+importThread();
+
+console.log("test");
--- ReportingB/importThread.js
+++ ReportingB/importThread.js
@@ -1,18 +1,9 @@
 /** Function to import a thread or subset of threads that was previously backed up
  *
- * @param fileNameh full file URI
+ * @param fileName full file URI
  * @return the JSON string containing the thread or subset of a thread that was backed up
  * */
 
-function importThread(fileName){
-    var request;
-    if (window.XMLHttpRequest) {
-        request = new XMLHttpRequest();
-    } else {
-        request = new ActiveXObject('Microsoft.XMLHTTP');
-    }
-    req.open('GET',fileName,false);
-    req.send();
-    var result = req.responseText;
-    return result;
+    module.exports = function(fileName){
+    console.log("export function");
 }
