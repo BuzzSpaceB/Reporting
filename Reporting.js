4f5fb8f6722d638047880990e43061193cb10cd8
JamesJJMurray <jjjmurray@gmail.com>
2015-03-24 22:54:47 +0200

    Changed some function names

--- Reporting.js
+++ Reporting.js
@@ -2,11 +2,11 @@
  * Created by User on 3/24/2015.
  */
 
-var myCallback = function(data) {
+var consolePrintCallback = function(data) {
         console.log('got data: '+data);
     };
 
 var importThread = require('./ReportingB/importThread.js');
-importThread("C:/New folder","backup.txt",myCallback);
+importThread("C:/New folder","backup.txt",consolePrintCallback);
 
 console.log("test");
