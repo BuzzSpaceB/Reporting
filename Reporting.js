4c0ba29bb1cb0c7a6987b7b6e6caf4482ad5bec5
u12003922 <u12003922@tuks.co.za>
2015-03-24 22:35:14 +0200

    Getting Files node ready
    Also added the unitTesting file

--- Reporting.js
+++ Reporting.js
@@ -1,7 +1,14 @@
 /**
  * Created by User on 3/24/2015.
  */
+ 
+ /** This loads all the functions from their files into the variables that should be used
+  *  to call the functions needed.
+  */
+ 
+var getThreadStats = require('./ReportingB/getThreadStats.js');
+var getThreadAppraisal = require('./ReportingB/getThreadAppraisal.js');
+var exportThreadAppraisal = require('./ReportingB/exportThreadAppraisal.js');
+var importThreadAppraisal = require('./ReportingB/importThreadAppraisal.js');
+var exportThread = require('./ReportingB/exportThread.js');
 var importThread = require('./ReportingB/importThread.js');
-importThread("thisName");
-
-console.log("test");
---
+++ UnitTesting.js
@@ -0,0 +1,8 @@
+/**
+ * Created by User on 3/19/2015.
+ */
+ 
+ /** This is where we do the unit testing for the Reporting Module
+  */
+  
+ var reporting = require('./Reporting.js');
