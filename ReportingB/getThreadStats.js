c93ea6fde7f62cbbc87f7fc2e15d6e024fe2a2b7
Carla de Beer <carla.debeer.uk@gmail.com>
2015-03-27 16:52:57 +0000

    Completed and tested getThreadStats function

--- ReportingB/getThreadStats.js
+++ ReportingB/getThreadStats.js
@@ -2,13 +2,13 @@
  * Carla de Beer
  * 95151835
  * COS 301: Reporting B: getThreadStats
- * 21.03.2015
+ * 27.03.2015
  */
 
 /**
  * The functionality provided by the getThreadStats function is to provide a versatile way to get statistical
- * information of subsets of posts complying with specfied restrictions. The set of posts returned by the
- * Threads.queryThread function is analysed according to the specfied "action" keyword.
+ * information of subsets of posts complying with specified restrictions. The set of posts returned by the
+ * Threads.queryThread function is analysed according to the specified "action" keyword.
  * @param posts a set of posts returned by the Threads.queryThread function
  * @param action action keyword
  * @param callback function to be called on result
@@ -17,19 +17,18 @@
 
 module.exports = function(posts, action, callback)
 {
-
     var JSONtoArray = []; // 1D array to store the number of JSON objects
 	var array = []; // 1D array to store all the details of the JSON object
     var arrayParentID = []; // 1D array to store the parent IDs
+	var arrayAuthorID = []; // 1D array to store the author IDs
 
-    // Parse the JSON object first
+    // Parse the incoming JSON object first
     var parsed = JSON.parse(posts);
 	
 	//console.log(parsed[0].ParentID);
 
-    // Fill the array to contain the content of the JSON object
+    // Fill "array" to contain the content of the parsed JSON object
     for(var i = 0; i < parsed.length; ++i){
-        //JSONtoArray.push(parsed[i]);
 		array.push(parsed[i].ParentID);
 		array.push(parsed[i].Author);
 		array.push(parsed[i].TimeStamp);
@@ -53,17 +52,16 @@ module.exports = function(posts, action, callback)
     else if (action === "MemCount"){
 
         var arrayAuthorID = []; // 1D array to store authorID array
-        var arrayAuthorIDNoDuplicates = []; // 1D array to store member array without duplicates
 
-        // Loop through array and get every 5th element, starting from index = 1
+        // Loop through array and get every 5th element (to get author list), starting from index = 1
         for (var i = 1; i < array.length; i+=5){
             arrayAuthorID.push(array[i]);
-        }
-		
-		//console.log(arrayAuthorID[0]);	
+        }	
+			
+		// Eliminate duplicated from arrayAuthorID
+		arrayAuthorID.unique();
 
-        arrayAuthorIDNoDuplicates = eliminateDuplicates(array);
-        callback(arrayAuthorIDNoDuplicates.length);
+        callback(arrayAuthorID.length);
     }
     else if(action == "MaxDepth"){
 
@@ -78,7 +76,26 @@ module.exports = function(posts, action, callback)
 };
 
 /**
- * Helper function that counts the number of parentIDs so get a value of the number of items in the dataset
+ * Helper function that eliminates duplicates from an array.
+ * @returns {this: array without duplicates}
+ */
+Array.prototype.unique = function() {
+    var a = [];
+    for (i = 0; i < this.length; i++) {
+        var current = this[i];
+        if (a.indexOf(current) < 0) a.push(current);
+    }
+
+    this.length = 0;
+    for (i = 0; i < a.length; i++) {
+        this.push(a[i]);
+    }
+
+    return this;
+}
+
+/**
+ * Helper function that counts the number of parentIDs so get a value of the number of items in the dataset.
  * @param array
  * @returns {Integer value that indicates the size of the array = number of items in dataset}
  */
@@ -93,25 +110,6 @@ function getNum(arr){
     return countArray.length;
 }
 
-/**
- * Helper function to eliminate duplicates in an array
- * @param arr
- * @returns {Array without duplicates}
- */
-function eliminateDuplicates(arr) {
-    var i,
-        len = arr.length,
-        out = [],
-        obj = {};
-
-    for (i = 0; i < len; i++) {
-        obj[arr[i]] = 0;
-    }
-    for (i in obj) {
-        out.push(i);
-    }
-    return out;
-}
 
 /**
  * Helper function that gets the maximum depth of a post.
@@ -121,7 +119,7 @@ function eliminateDuplicates(arr) {
  */
 function getMaxDepth(array,pID ){
 
-    // Loop through the array to get each 5th element, i.e. the parentID
+    // Loop through the array to get each 5th element, i.e. create the parentID array
     for (var i = 0; i < array.length; i+=5){
         pID.push(array[i]);
     }
@@ -129,11 +127,44 @@ function getMaxDepth(array,pID ){
     // Sorting the array first so that the first item can be accessed as the most frequent
     pID.sort();
 
+	// Count the number of occurrences in the array
     var obj = {};
     for (var i = 0, j = pID.length; i < j; ++i) {
         obj[pID[i]] = (obj[pID[i]] || 0) + 1;
     }
 
+	//console.log(obj['0']); 
+	
     // Return the count value of the first item (= most frequent) in the object
-    return obj[0]; // not entirely sure about this ...
+    return obj[0]; 
+}
+
+/**
+ * Helper function that gets the average depth of a post.
+ * @param array
+ * @param pID
+ * @param aID
+ * @returns {Integer value associated with the average depth of a post}
+ */
+function getAveDepth(array, pID, aID) {
+
+    // Create parentID array:
+    // Loop through main array and get every 5th element, starting from index = 0
+    for (var i = 0; i < array.length; i+=5){
+        pID.push(array[i]);
+    }
+
+    // Create authorID array:
+    // Loop through main array and get every 5th element, starting from index = 1
+    for (var i = 1; i < array.length; i+=5){
+        aID.push(array[i]);
+    }
+
+    // Remove duplicates from authorID array
+    aID.unique();
+
+    var numParent = pID.length;
+    var numAuthor = aID.length;
+
+    return numParent / numAuthor;
 }
