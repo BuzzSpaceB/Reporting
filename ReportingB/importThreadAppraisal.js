1ba1e5d1dfb3204d38e1c6e1c52694caacf44803
pepezane <dittodollz@gmail.com>
2015-03-27 14:25:50 +0200

    Added comments

--- ReportingB/importThreadAppraisal.js
+++ ReportingB/importThreadAppraisal.js
@@ -1,79 +1,127 @@
-/**Edited by Mkhabela Phethile 3/19/2015**/
-/**the importThreadAppraisal Function gets a csv file as input from the exportThreadAppraisal function, the function then checks that the member and the appraisal are valid, calls assignAppraisalToPost for each field if valid and throws an exception if invalid**/
-/**This function requires you to run in on a server like Apache, I used usbwebserver**/
-function importThreadAppraisal(filename)//function gets the name of the file as input same file from exportThreadAppraisal(),this is how i achieved the functionality <a href="#" onclick="importThreadAppraisal('yes.csv')">test</a>
-{
-   /**variable used for request purposes**/
-  var req;
-    if (window.XMLHttpRequest) {
-        /**supported by the following browsers: IE7+, Firefox, Chrome, Opera, Safari**/
-        req = new XMLHttpRequest();
-    } else {
-        /**for IE6, IE5**/
-        req = new ActiveXObject('Microsoft.XMLHTTP');
-    }
-    /**this part loads the file, using GET**/
-    req.open('GET',filename,false);
-    req.send();
-    /**storing the request's response text to the data variable**/
-    var data = req.responseText;
+/**
+ * Mkhabela Phethile
+ * 12097561
+ * COS 301: Reporting B: importThreadAppraisal
+ * 3/27/2015
+ */
+/**
+*The importThreadAppraisal Function gets a csv file as input from the exportThreadAppraisal function
+*The function then checks that the member and the appraisal are valid
+*calls assignAppraisalToPost for each field if valid and throws an exception if invalid
+* @param directory where the csv file is saved
+* @param fileName the name of the csv file
+*/
 
-parseData(data);
-}
-/**parse csv**/ 
+module.exports = function(directory,fileName)
+{
+    //variable to store the files content
+    var dataRead;
+	//variable fs for reading in the file
+    var fs = require('fs');
+	//variable path to store the path to file
+    var path = require('path');
+	//variable filepath to store the full path including the directory and filename requested
+    var filePath = path.join(directory,fileName);
+	//storing the data from the file into dataRead and calling the parseData function to parse the data
+    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+        if (!err){
+		//storing data into dataread
+            dataRead = data;
+			//calling parse data
+            parseData(dataRead);
+        }else{
+		//Incase the is an error, shows the error on the console
+            console.log(err);
+        }
+    });
+
+
+};
+
+
+
+
+ /** Function to parse the csv data
+     *@param data - the variable which contains the csv files contents
+     * */ 
 function parseData(data)
 {
 
-/**replace UNIX new lines**/
+//replace UNIX new lines
 data = data.replace (/\r\n/g, "\n");
-/**replace MAC new lines**/
+//replace MAC new lines
 data = data.replace (/\r/g, "\n");
-/**split csv data into rows**/
+//split csv data into rows
 var rows = data.split("\n");
 
 for (var i = 0; i < rows.length; i++) {
 
-  /** this line helps to skip empty rows**/
+  // this line helps to skip empty rows
     if (rows[i]) {
-        /**our columns are separated by comma**/
+        //our columns are separated by comma
         var column = rows[i].split(",");
 		
-		/**column is an array now, the first item is the parent id**/
+		//column is an array now, the first item is the parent id
 		var parentId=column[0];
-		/**The second item is the author**/ 
+		//The second item is the author 
 		var author=column[1];
-		/**the third item is the timestamp**/
+		//the third item is the timestamp
 		var timestamp=column[2];
-		/**the fourth is the content**/
+		//the fourth is the content
 		var content=column[3];
-		/**the fifth is the status**/
+		//the fifth is the status
 		var status=column[4];
-		/**the sixth is th memberID**/
+		//the sixth is th memberID
 		var memberId=column[5];
-		/**the seventh is the appraisal id**/
+		//the seventh is the appraisal id
 		var appraisalId=column[6];
-		/**the eight is the appraisal value**/
+		//the eight is the appraisal value
 		var appraisalValue=column[7];
 
-	/**the isAuthorised function is out of my scope along with the assignAppraisalToPost**/
+	//the isAuthorised function is out of my scope along with the assignAppraisalToPost
 	if(isValid(appraisalValue)&&isAuthorised(memberId)) assignAppraisalToPost(appraisalValue);
 	}
 }
 }
 
-/**function that checks if the appraisal value are valid**/
+
+ /** function that checks if the appraisal value are valid
+     *@param appraisalValue
+     * */ 
 function isValid(appraisalValue)
 {
 try {
-  /**I was not sure what  the highest appraisal value could be so i decided on 10, you may alter this if you wish**/
-	if(appraisalValue == "") throw "empty"; else return true;
+  //I was not sure what  the highest appraisal value could be so i decided on 10, you may alter this if you wish
+    if ((appraisalValue !=="")&&(appraisalValue>0)&&(appraisalValue<10))
+    {
+
+        return true;
+    }
+    else
+    {
+
+        return false;
+    }
+	/*if(appraisalValue == "") throw "empty"; else return true;
         if(isNaN(appraisalValue)) throw "not a number"; else return true;
         if(appraisalValue < 1) throw "too low"; else return true;
-        if(appraisalValue > 10) throw "too high"; else return true;
+        if(appraisalValue > 10) throw "too high"; else return true;*/
    }
 catch(err) {
       message.innerHTML = "Appraisal Value is " + err;
+    }
 }
+
+
+ /** This function is a dummy one since we dont implement it.
+     *@param someData- which is should be the Member ID
+     * */ 
+function isAuthorised(someData)
+{return true;}
+
+function assignAppraisalToPost(postValue) //Dummy Function
+{
+console.log("Appraisal post value " + postValue);
 }
 
 
