b6be427f93e38b13a36e1e134a4e52820733c2c6
Carla de Beer <carla.debeer.uk@gmail.com>
2015-03-28 09:31:36 +0000

    Further testing and amendments to getThreadStats

--- ReportingB/getThreadStats.js
+++ ReportingB/getThreadStats.js
@@ -2,7 +2,7 @@
  * Carla de Beer
  * 95151835
  * COS 301: Reporting B: getThreadStats
- * 27.03.2015
+ * 28.03.2015
  */
 
 /**
@@ -12,12 +12,11 @@
  * @param posts a set of posts returned by the Threads.queryThread function
  * @param action action keyword
  * @param callback function to be called on result
- * @returns {integer or double value corresponding to the action input}
+ * @returns {integer or floating point value corresponding to the action input}
  */
 
-module.exports = function(posts, action, callback)
-{
-    var JSONtoArray = []; // 1D array to store the number of JSON objects
+module.exports = function(posts, action, callback) {
+
 	var array = []; // 1D array to store all the details of the JSON object
     var arrayParentID = []; // 1D array to store the parent IDs
 	var arrayAuthorID = []; // 1D array to store the author IDs
@@ -27,7 +26,8 @@ module.exports = function(posts, action, callback)
 	
 	//console.log(parsed[0].ParentID);
 
-    // Fill "array" to contain the content of the parsed JSON object
+    // Fill array to contain the content of the parsed JSON object
+    // (from here on we work with array, not the JSON object)
     for(var i = 0; i < parsed.length; ++i){
 		array.push(parsed[i].ParentID);
 		array.push(parsed[i].Author);
@@ -66,14 +66,28 @@ module.exports = function(posts, action, callback)
     else if(action == "MaxDepth"){
 
         callback(getMaxDepth(array,arrayParentID));
-
     }
     else if(action === "AvgDepth"){
 
-        callback(getAveDepth(array, arrayParentID, arrayAuthorID));
-
+        callback(getAveDepth(array, arrayParentID));
     }
 };
+
+/**
+ * Helper function that counts the number of parentIDs so get a value of the number of items in the dataset.
+ * @param array
+ * @returns {Integer value that indicates the size of the array = number of items in dataset}
+ */
+function getNum(arr){
+
+    var countArray = [];
+
+    for(var i = 0; i < arr.length; i+=5){
+        countArray.push(arr[i]);
+    }
+
+    return countArray.length;
+}
 
 /**
  * Helper function that eliminates duplicates from an array.
@@ -95,23 +109,6 @@ Array.prototype.unique = function() {
 }
 
 /**
- * Helper function that counts the number of parentIDs so get a value of the number of items in the dataset.
- * @param array
- * @returns {Integer value that indicates the size of the array = number of items in dataset}
- */
-function getNum(arr){
-
-    var countArray = [];
-
-    for(var i = 0; i < arr.length; i+=5){
-        countArray.push(arr[i]);
-    }
-
-    return countArray.length;
-}
-
-
-/**
  * Helper function that gets the maximum depth of a post.
  * @param array
  * @param arrayParentIDs
@@ -124,19 +121,28 @@ function getMaxDepth(array,pID ){
         pID.push(array[i]);
     }
 
-    // Sorting the array first so that the first item can be accessed as the most frequent
+    // Set of two arrays, a and b:
+    // a holds the patentIDs, b counts the number of occurrences
+	var a = [], b = [], prev;
+
+    // Sort the parentID array first
     pID.sort();
 
-	// Count the number of occurrences in the array
-    var obj = {};
-    for (var i = 0, j = pID.length; i < j; ++i) {
-        obj[pID[i]] = (obj[pID[i]] || 0) + 1;
+    // Do the number crunching
+    for ( var i = 0; i < pID.length; i++ ) {
+        if ( pID[i] !== prev ) {
+            a.push(pID[i]);
+            b.push(1);
+        } else {
+            b[b.length-1]++;
+        }
+        prev = pID[i];
     }
 
-	//console.log(obj['0']); 
-	
-    // Return the count value of the first item (= most frequent) in the object
-    return obj[0]; 
+    // return [a, b];
+
+    // Return the occurrence with the highest value
+    return Math.max.apply(Math, b);
 }
 
 /**
@@ -144,9 +150,9 @@ function getMaxDepth(array,pID ){
  * @param array
  * @param pID
  * @param aID
- * @returns {Integer value associated with the average depth of a post}
+ * @returns {Floating point value associated with the average depth of a post}
  */
-function getAveDepth(array, pID, aID) {
+function getAveDepth(array, pID) {
 
     // Create parentID array:
     // Loop through main array and get every 5th element, starting from index = 0
@@ -154,17 +160,26 @@ function getAveDepth(array, pID, aID) {
         pID.push(array[i]);
     }
 
-    // Create authorID array:
-    // Loop through main array and get every 5th element, starting from index = 1
-    for (var i = 1; i < array.length; i+=5){
-        aID.push(array[i]);
+    // Set of two arrays, a and b:
+    // a holds the patentIDs, b counts the number of occurrences
+    var a = [], b = [], prev;
+
+    // Sort the parentID array first
+    pID.sort();
+
+    // Do the number crunching
+    for ( var i = 0; i < pID.length; i++ ) {
+        if ( pID[i] !== prev ) {
+            a.push(pID[i]);
+            b.push(1);
+        } else {
+            b[b.length-1]++;
+        }
+        prev = pID[i];
     }
 
-    // Remove duplicates from authorID array
-    aID.unique();
+    //console.log([a, b]);
 
-    var numParent = pID.length;
-    var numAuthor = aID.length;
-
-    return numParent / numAuthor;
+    // Return floating point value to 3 decimal places
+    return (getNum(array) / a.length).toFixed(3);
 }
--- UnitTests.js
+++ UnitTests.js
@@ -67,10 +67,12 @@ function testGetThreadStats(){
     var postList = [
     {"ParentID":"0", "Author":"1", "TimeStamp":"2015.03.15", "Content":"This is the first post I am writing here. My name is Eric.", "Status":"visible"},
     {"ParentID":"1", "Author":"2", "TimeStamp":"2015.03.17", "Content":"Hi! My name is Jonah.", "Status":"visible"},
-    {"ParentID":"1", "Author":"0", "TimeStamp":"2015.03.17", "Content":"My name is Sarah. I am studying Computer Science", "Status":"visible"},
+    {"ParentID":"2", "Author":"3", "TimeStamp":"2015.03.17", "Content":"My name is Sarah. I am studying Computer Science", "Status":"visible"},
     {"ParentID":"2", "Author":"1", "TimeStamp":"2015.03.18", "Content":"I am also studying Computer Science.", "Status":"visible"},
-    {"ParentID":"3", "Author":"2", "TimeStamp":"2015.03.18", "Content":"I wrote something that caused my post to be made hidden.", "Status":"hidden"}
-	];	
+    {"ParentID":"2", "Author":"2", "TimeStamp":"2015.03.18", "Content":"I wrote something that caused my post to be made hidden.", "Status":"hidden"},
+	{"ParentID":"2", "Author":"0", "TimeStamp":"2015.03.19", "Content":"I am learning JavaScript.", "Status":"visible"},
+	{"ParentID":"2", "Author":"0", "TimeStamp":"2015.03.19", "Content":"I am learning Java.", "Status":"visible"}
+	];
 	
 	var jsonObject = JSON.stringify(postList);
 	   
